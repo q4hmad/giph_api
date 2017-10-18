9208e9aa7646838661360940895c1a534ce51cf9
Qudsia Ahmad and Rebecca MacIntosh <qudsia@>
2017-10-18 16:37:55 -0700

    return bike colors using Promises with an API

--- js/word-interface.js
+++ js/word-interface.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
       url: `https://api.giphy.com/v1/gifs/search?q=${paragraph}&api_key=F36raDuh8TNRI0N04DlPSZWSA3Iitj11 `,
       type: 'GET',
       data: {
-        // format: 'json'
+        format: 'json'
       },
       success: function(response) {
         let img = response.data[0].images.fixed_width.url;
